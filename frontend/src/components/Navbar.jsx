f1f64017e0625f352deb6fa4946dbf905ece2934
James Phillip Petrie <jamesppetrie@hotmail.com>
2023-11-26 10:22:33 +1100

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,16 @@ function Navbar() {
 
   return (
     <>
+    <nav className='navbar'>
+      <h1 className='item-header-banner'>Welcome</h1>
+      <ul>
+        <li>
+          <a href='/portfolio'>Portfolio</a>
+        </li>
+      </ul>
+    </nav>
       <div className='container-header'>
-          <h1 className='item-header-banner'>Welcome</h1>
+         
       </div>
     </>
   )
